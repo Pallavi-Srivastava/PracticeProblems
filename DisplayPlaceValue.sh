2769ee1ae75a115406950dd4480b437827d39a6c
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-08-29 08:59:56 +0530

    Adding Day5 Practice Problem

--- DisplayPlaceValue.sh
+++ DisplayPlaceValue.sh
@@ -1,159 +1,34 @@
-#!/bin/bash -x 
-read -p "Enter the price:" price
-
-thousands=$((price/1000))
-hundreds=$((price%1000/100))
-teens=$((price%100/10))
-units=$((price%10))
-
-
-for (( i=0 ; i<=$thousands; i++ ))
-do
-if [ $thousands -eq 0 ]
+#!/bin/bash -x
+read -p "Enter Any number:" n
+if [ $n -gt 0 ] && [ $n -le 9 ]
 then
-	echo ""
-elif [ $thousands -eq 1 ]
+   u=$((n%10))
+   echo " the units place is $u"
+elif [ $n -ge 10 ] && [ $n -le 99 ]
 then
-	echo "one thousand"
-elif [ $thousands -eq 2 ]
+   	u=$((n%10))
+        t=$(( (n/10)%10 ))
+        echo "The tens place is $t"
+	echo "The units place is $u"
+elif [ $n -ge 100 ] && [ $n -le 999 ]
 then
-	echo "two thousand"
-elif [ $thousands -eq 3 ]
+       u=$((n%10))
+       t=$(( (n/10)%10 ))
+       h=$(( (n/100)%10 ))
+       echo " The hundreds place is $h"
+       echo " The tens place is $t"
+       echo " The units place is $u"
+elif [ $n -ge 1000 ] && [ $n -le 9999 ]
 then
-	echo "three thousand"
-elif [ $thousands -eq 4 ]
-then
-        echo "Four thousand"
-elif [ $thousands -eq 5 ]
-then
-	echo "Five thousand"
-elif [ $thousands -eq 6 ]
-then
-	echo "Six thousand"
-elif [ $thousands -eq 7 ]
-then
-	echo "seven thousands"
-elif [ $thousands -eq 8 ]
-then
-	echo "eight thousands"
-elif [ $thousands -eq 9 ]
-then
-	echo "nine thousands"
-elif [ $thousands -eq 10 ]
-then
-	echo "ten thousands"
+        u=$((n%10))
+        t=$(( (n/10)%10 ))
+        h=$(( (n/100)%10 ))
+        th=$((n/1000));
+	echo "The  thousands place is $th"
+	echo " The hundreds place is $h"
+   	echo " The tens place is $t"
+   	echo " The units place is $u"
 fi
-done
-for (( i=0 ; i<=$hundreds; i++ ))
-do
-if [ $hundreds -eq 0 ]
-then
-        echo ""
-elif [ $hundreds -eq 1 ]
-then
-        echo "one hundred"
-elif [ $hundreds -eq 2 ]
-then
-        echo "two hundred"
-elif [ $hundreds -eq 3 ]
-then
-        echo "three hundred"
-elif [ $hundreds -eq 4 ]
-then
-        echo "Four hundred"
-elif [ $hundreds -eq 5 ]
-then
-        echo "Five hundred"
-elif [ $hundreds -eq 6 ]
-then
-        echo "Six hundred"
-elif [ $hundreds -eq 7 ]
-then
-        echo "seven hundred"
-elif [ $hundreds -eq 8 ]
-then
-        echo "eight hundred"
-elif [ $hundreds -eq 9 ]
-then
-        echo "nine hundred"
-elif [ $hundreds -eq 10 ]
-then
-        echo "ten hundred"
-fi
-done
-for (( i=0 ; i<=$teens; i++ ))
-do
-if [ $teens -eq 0 ]
-then
-        echo ""
-elif [ $teens -eq 1 ]
-then
-        echo "ten"
-elif [ $teens -eq 2 ]
-then
-        echo "twenty"
-elif [ $teens -eq 3 ]
-then
-        echo "thirty"
-elif [ $teens -eq 4 ]
-then
-        echo "Fourty"
-elif [ $teens -eq 5 ]
-then
-        echo "Fivty"
-elif [ $teens -eq 6 ]
-then
-        echo "Sixty"
-elif [ $teens -eq 7 ]
-then
-        echo "sevety"
-elif [ $teens -eq 8 ]
-then
-        echo "eighty"
-elif [ $teens -eq 9 ]
-then
-        echo "ninty"
-elif [ $teens -eq 10 ]
-then
-        echo "hundreds"
-fi
-done
-for (( i=0 ; i<=$units; i++ ))
-do
-if [ $units -eq 0 ]
-then
-        echo ""
-elif [ $units -eq 1 ]
-then
-        echo "one"
-elif [ $units -eq 2 ]
-then
-        echo "two"
-elif [ $units -eq 3 ]
-then
-        echo "three"
-elif [ $units -eq 4 ]
-then
-        echo "Four"
-elif [ $units -eq 5 ]
-then
-        echo "Five"
-elif [ $units -eq 6 ]
-then
-        echo "Six"
-elif [ $units -eq 7 ]
-then
-        echo "seven"
-elif [ $units -eq 8 ]
-then
-        echo "eight"
-elif [ $units -eq 9 ]
-then
-        echo "nine"
-elif [ $units -eq 10 ]
-then
-        echo "ten"
-fi
-done
 
-echo "The price is: " `expr $thousands + $hundreds + $teens + $units`
+
+
